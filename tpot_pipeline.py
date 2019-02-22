b67a548a8a2a35e1dd09d83fdc2a4b4251f80f02
Karol <kajezierski@wp.pl>
2019-02-22 22:04:52 +0100

    Finished project

--- tpot_pipeline.py
+++ tpot_pipeline.py
@@ -13,13 +13,22 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Average CV score on the training set was:0.7622888546486963
+# Average CV score on the training set was:0.7648445932689355
 exported_pipeline = make_pipeline(
     make_union(
-        FunctionTransformer(copy),
-        StackingEstimator(estimator=LGBMClassifier(learning_rate=0.013093533273536108, max_depth=2, n_estimators=417, random_state=42))
+        make_union(
+            FunctionTransformer(copy),
+            FunctionTransformer(copy)
+        ),
+        StackingEstimator(estimator=make_pipeline(
+            make_union(
+                FunctionTransformer(copy),
+                FunctionTransformer(copy)
+            ),
+            LGBMClassifier(learning_rate=0.01193776641714437, max_depth=4, n_estimators=1122, random_state=42)
+        ))
     ),
-    LGBMClassifier(learning_rate=0.002015337685941733, max_depth=3, n_estimators=1265, random_state=42)
+    LGBMClassifier(learning_rate=0.026797460873256924, max_depth=3, n_estimators=216, random_state=42)
 )
 
 exported_pipeline.fit(training_features, training_target)
